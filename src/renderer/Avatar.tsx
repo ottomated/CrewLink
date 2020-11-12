f2356074d2825bfc9a52f367a4be4fd45ce9ef86
Ottomated <otto@ottomated.net>
2020-11-11 16:54:47 -0800

    Disable cosmetics on ghosts

--- src/renderer/Avatar.tsx
+++ src/renderer/Avatar.tsx
@@ -14,6 +14,7 @@ export interface CanvasProps {
 	skin: number;
 	color: string;
 	shadow: string;
+	isAlive: boolean;
 }
 
 export interface AvatarProps {
@@ -45,24 +46,24 @@ export default function Avatar({ talking, deafened, borderColor, isAlive, player
 	if (!color) color = playerColors[0];
 	return (
 		<Tooltip useHover={!player.isLocal} content={player.name} padding={5}>
-		<div className="avatar" style={{
-			borderColor: talking ? borderColor : 'transparent',
-			borderWidth: Math.max(2, size / 40),
-			width: size,
-			height: size
-		}}>
-			<Canvas src={isAlive ? alive : dead} hat={player.hatId - 1} skin={player.skinId - 1} color={color[0]} shadow={color[1]} />
-			{
-				deafened &&
-				<svg viewBox="0 0 24 24" fill="white" width="28px" height="28px"><path d="M0 0h24v24H0z" fill="none" /><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" /></svg>
-			}
-		</div>
-			
-			</Tooltip>
+			<div className="avatar" style={{
+				borderColor: talking ? borderColor : 'transparent',
+				borderWidth: Math.max(2, size / 40),
+				width: size,
+				height: size
+			}}>
+				<Canvas src={isAlive ? alive : dead} hat={player.hatId - 1} skin={player.skinId - 1} isAlive={isAlive} color={color[0]} shadow={color[1]} />
+				{
+					deafened &&
+					<svg viewBox="0 0 24 24" fill="white" width="28px" height="28px"><path d="M0 0h24v24H0z" fill="none" /><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" /></svg>
+				}
+			</div>
+
+		</Tooltip>
 	);
 }
 
-function Canvas({ src, hat, skin, color, shadow }: CanvasProps) {
+function Canvas({ src, hat, skin, color, shadow, isAlive }: CanvasProps) {
 	const canvas = useRef<HTMLCanvasElement>(null);
 	const hatImg = useRef<HTMLImageElement>(null);
 	const skinImg = useRef<HTMLImageElement>(null);
@@ -74,7 +75,7 @@ function Canvas({ src, hat, skin, color, shadow }: CanvasProps) {
 
 		canvas.current.width = image.current.width;
 		canvas.current.height = image.current.height;
-		console.log(backLayerHats, hat - 1);
+		ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
 		ctx.drawImage(image.current, 0, 0);
 
 		function drawHat() {
@@ -99,14 +100,16 @@ function Canvas({ src, hat, skin, color, shadow }: CanvasProps) {
 			}
 		}
 		ctx.putImageData(data, 0, 0);
-		if (backLayerHats.has(hat))
-			ctx.globalCompositeOperation = 'destination-over';
-		drawHat();
-		ctx.globalCompositeOperation = 'source-over';
+		if (isAlive) {
+			if (backLayerHats.has(hat))
+				ctx.globalCompositeOperation = 'destination-over';
+			drawHat();
+			ctx.globalCompositeOperation = 'source-over';
 
-		ctx.drawImage(skinImg.current, 25, 46);
+			ctx.drawImage(skinImg.current, 25, 46);
+		}
 
-	}, [src, color, shadow, hat, skin]);
+	}, [src, color, shadow, hat, skin, isAlive]);
 
 	return (
 		<>
