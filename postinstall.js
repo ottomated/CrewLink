25b9df2beee9b9305114e0de4fcda8b6abb0726e
Ben Leach <benleach@pm.me>
2020-12-02 22:20:39 -0500

    working postinstall

--- postinstall.js
+++ postinstall.js
@@ -4,5 +4,5 @@ const path = require('path')
 const dest = path.join(__dirname, 'node_modules/iohook/builds/electron-v80-win32-x64/build/Release')
 
 fs.mkdirSync(dest, { recursive: true })
-fs.copyFileSync(path.join(__dirname, 'iohook/iohook.node'), dest)
-fs.copyFileSync(path.join(__dirname, 'iohook/uiohook.dll'), dest)
+fs.copyFileSync(path.join(__dirname, 'iohook/iohook.node'), path.join(dest, 'iohook.node'))
+fs.copyFileSync(path.join(__dirname, 'iohook/uiohook.dll'), path.join(dest, 'uiohook.dll'))
