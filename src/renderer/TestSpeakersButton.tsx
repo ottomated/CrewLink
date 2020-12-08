30205e9e33fe4dc5c4774c1888e2516935465609
leny32 <40176071+leny32@users.noreply.github.com>
2020-12-08 15:10:19 +0100

    Corrected a mistake

--- src/renderer/TestSpeakersButton.tsx
+++ src/renderer/TestSpeakersButton.tsx
@@ -2,21 +2,26 @@ import React, { useContext } from 'react'
 import { SettingsContext } from './App';
 // @ts-ignore
 import chime from '../../static/chime.mp3';
+let audio;
 
 const TestSpeakersButton = () => {
     const [{ speaker }] = useContext(SettingsContext)
-
-    const audio = new Audio(chime);
     
     const testSpeakers = () => {
 
         if (speaker.toLowerCase() !== 'default')
             (audio as any).setSinkId(speaker)
 
-        if (!isPlaying(audio)) audio.play();
+            if (!audio) audio = getAudio();
+            if (!isPlaying(audio)) audio.play();
     }
 
     return <button className="test-speakers" onClick={testSpeakers}>Test Speaker</button>
+}
+
+function getAudio(audio) {
+    if (!audio) audio = new Audio(chime);
+    return audio;
 }
 
 function isPlaying(audio) {
