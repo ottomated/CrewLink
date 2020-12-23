c1b05ce795a1ca78096b480c49799c4a9f0e8293
OhMyGuus <info@guus.ninja>
2020-12-23 10:26:56 +0100

    Small fix for gamecode.

--- src/main/GameReader.ts
+++ src/main/GameReader.ts
@@ -90,7 +90,7 @@ export default class GameReader {
 					break;
 			}
 
-			this.gameCode = this.IntToGameCode(this.readMemory<number>('int32', this.gameAssembly.modBaseAddr, this.offsets.gameCode));
+			this.gameCode = state === GameState.MENU? "" : this.IntToGameCode(this.readMemory<number>('int32', this.gameAssembly.modBaseAddr, this.offsets.gameCode));
 			const allPlayersPtr = this.readMemory<number>('ptr', this.gameAssembly.modBaseAddr, this.offsets.allPlayersPtr);
 			const allPlayers = this.readMemory<number>('ptr', allPlayersPtr, this.offsets.allPlayers);
 			const playerCount = this.readMemory<number>('int' as const, allPlayersPtr, this.offsets.playerCount);
@@ -241,7 +241,7 @@ export default class GameReader {
 	}
 
 	IntToGameCode(input: number) {
-		if (input === 0 || input > -1000)
+		if (!input || input === 0 || input > -1000)
 			return "";
 
 		const V2: string = "QWXRTYLPESDFGHUJKZOCVBINMA";
